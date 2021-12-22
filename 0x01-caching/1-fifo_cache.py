8d11a4e0582022091fc6d1bc6709b068a87aa697
Erick Kiminza <kiminzaeric@gmail.com>
2021-12-22 10:40:43 +0300

    fifo caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,61 +1,10 @@
-#!/usr/bin/python3
-""" BaseCaching module
+#!/usr/bin/env python3
+""" FIFOCache inherits from BasicCache
 """
+BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError(
-            "put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError(
-            "get must be implemented in your cache class")
-
-
-class BasicCache(BaseCaching):
-    """ Inherits from BaseCaching
-    """
-
-    def put(self, key, item):
-        """ Add an item to cache"""
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-
-    def get(self, key):
-        """ Get an item by key"""
-        if key is None:
-            return None
-        try:
-            self.cache_data[key]
-        except KeyError:
-            return None
-        return self.cache_data[key]
-
-
-class FIFOCache(BasicCache):
+class FIFOCache(BaseCaching):
     """ inherits from BasicCache
     """
 
